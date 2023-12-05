4e70e75c5309989f0b0a5d35f1c6b1295d585404
RahulVarma1234 <chamarthirahul2105@gmail.com>
2023-12-05 18:01:58 +0530

    added

--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 8: Making scoreboard functional
+let scoreboard = document.getElementById('score-board');
+scoreboard.innerText = localStorage.getItem('score');
+
+let playAgainBtn = document.getElementById('play-again-button');
+playAgainBtn.addEventListener('click', function (){
+    window.location.href = './game.html';
+});
